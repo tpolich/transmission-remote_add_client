b0db4c6687f331c0907ad98235971b47ec013d69
tpolich <tpolich@gmail.com>
2013-01-19 21:31:57 -0600

    added support for .torrent files

--- add_torrent.py
+++ add_torrent.py
@@ -4,27 +4,33 @@ import json
 import requests
 import sys
 import os
+import base64
 
 #load setting from the config.json file sitting in the same folder as this programs
-settings = json.load(open(os.path.dirname(os.path.realpath(__file__))+'/config.json'))
+st = json.load(open(os.path.dirname(os.path.realpath(__file__))+'/config.json'))
 
-username = settings['username']
-password = settings['password']
-url = settings['url']
-torrent_url = sys.argv[1]
 headers = {'content-type':'application/json'}
-payload = {
-           'arguments': {
-				 'filename': torrent_url
-	         },
-	         'method': 'torrent-add',
-	      }
-		  
-r = requests.post(url,auth=(username,password),data=json.dumps(payload),headers=headers)
+payload = {'arguments': {}, 'method': 'torrent-add'}
+
+#if the file passed in has magnet:? in the name we assume it's a magnet link being passed in
+if "magnet:?" in sys.argv[1]:
+	payload['arguments']['filename'] = sys.argv[1]
+	
+
+#anything else is taken as a torrent file	
+else:
+	#open the torrent file
+	f = open(sys.argv[1],'rb')
+	#encode the torrent file as base64, make it a string, ripe off the extra crap on the front and tail
+	payload['arguments']['metainfo'] = str(base64.b64encode(f.read()))[2:][:-1]
+	f.close()
+	
+#make the add torrent POST
+r = requests.post(st['url'],auth=(st['username'],st['password']),data=json.dumps(payload),headers=headers)
 
 #if there is a 409 error get the session id and retry
 if "409: Conflict" in r.text:
 	#parsing the id out of the response. dirty but easy
 	headers['X-Transmission-Session-Id'] = r.text.split(': ')[2][:-11]
-	r = requests.post(url,auth=(username,password),data=json.dumps(payload),headers=headers)
+	r = requests.post(st['url'],auth=(st['username'],st['password']),data=json.dumps(payload),headers=headers)
 	
